66e69e51e588217b43b517eedf144e478b6ab8e5
SpartanPlume <spartan.plume@gmail.com>
2018-04-20 23:22:43 +0900

    Update brackets endpoint

--- server/routes/v1/tosurnament/tournaments/brackets/all.py
+++ server/routes/v1/tosurnament/tournaments/brackets/all.py
@@ -1,6 +1,29 @@
 """Route to all brackets"""
 
+from databases.bracket import Bracket
+
 def get(handler, parameters, url_parameters, ids_parameters):
-    print(parameters)
-    print(url_parameters)
-    print(ids_parameters)
+    """GET method"""
+    [tournament_id] = ids_parameters
+    result = handler.session.query(Bracket).where(Bracket.tournament_id == tournament_id).all()
+    if result:
+        print("GET: all: brackets: "str(len(result)) + " results")
+        handler.send_array(result)
+    else:
+        print("GET: all: brackets: No result")
+        handler.send_json("{}")
+
+def post(handler, parameters, url_parameters, ids_parameters):
+    """POST method"""
+    [tournament_id] = ids_parameters
+    if not parameters:
+        print("POST: all: brackets: Ignoring")
+        handler.send_json("{}")
+        return
+    obj = Bracket()
+    for key, value in parameters.items():
+        if key in obj.__dict__:
+            setattr(obj, key, value)
+    obj = handler.session.add(obj)
+    print("POST: all: brackets: Bracket created")
+    handler.send_object(obj)
--- server/routes/v1/tosurnament/tournaments/brackets/single.py
+++ server/routes/v1/tosurnament/tournaments/brackets/single.py
@@ -1,8 +1,48 @@
 """Route to a single bracket"""
 
+from databases.bracket import Bracket
+from databases.players_spreadsheet import PlayersSpreadsheet
+from databases.schedules_spreadsheet import SchedulesSpreadsheet
+
 def get(handler, parameters, url_parameters, ids_parameters):
-    print(parameters)
-    print(url_parameters)
+    """GET method"""
     [tournament_id, bracket_id] = ids_parameters
-    print(tournament_id)
-    print(bracket_id)
+    result = handler.session.query(Bracket).where(Bracket.tournament_id == int(tournament_id)).where(Bracket.id == int(bracket_id)).first()
+    if result:
+        print("GET: single: brackets: 1 result for " + bracket_id)
+        players_spreadsheet = handler.session.query(PlayersSpreadsheet).where(PlayersSpreadsheet.id == result.players_spreadsheet_id).first()
+        schedules_spreadsheet = handler.session.query(SchedulesSpreadsheet).where(SchedulesSpreadsheet.id == result.schedules_spreadsheet_id).first()
+        result.players_spreadsheet = players_spreadsheet.get_dict()
+        result.schedules_spreadsheet = schedules_spreadsheet.get_dict()
+        handler.send_object(result)
+    else:
+        print("GET: single: brackets: This bracket does not exist")
+        handler.send_json("{}")
+
+def put(handler, parameters, url_parameters, ids_parameters):
+    """PUT method"""
+    [tournament_id, bracket_id] = ids_parameters
+    if not parameters:
+        print("PUT: single: brackets: Ignoring")
+        handler.send_json("{}")
+        return
+    result = handler.session.query(Bracket).where(Bracket.tournament_id == int(tournament_id)).where(Bracket.id == int(bracket_id)).first()
+    if not result:
+        print("PUT: single: brackets: This bracket does not exist")
+        handler.send_json("{}")
+        return
+    handler.session.update_columns(Bracket, bracket_id, parameters)
+    print("PUT: single: brackets: Bracket updated")
+    handler.send_json("{}")   
+
+def delete(handler, parameters, url_parameters, ids_parameters):
+    """DELETE method"""
+    [tournament_id, bracket_id] = ids_parameters
+    result = handler.session.query(Bracket).where(Bracket.tournament_id == int(tournament_id)).where(Bracket.id == int(bracket_id)).first()
+    if not result:
+        print("PUT: single: brackets: This bracket does not exist")
+        handler.send_json("{}")
+        return
+    handler.session.delete(result)
+    print("DELETE: single: Bracket " + bracket_id + " deleted")
+    handler.send_json("{}")
